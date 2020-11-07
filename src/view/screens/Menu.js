b697bd736a789af5dd2694864215341fad6671c6
chanhee-lee <clee643@gatech.edu>
2020-11-06 22:50:12 -0500

    temporary tree view

--- src/view/screens/Menu.js
+++ src/view/screens/Menu.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import Tree from  './Tree';
 
 export default function Menu() {
   return (
-    <Text>Menu</Text>
+      <View>
+        <Text>Menu</Text>
+      </View>
   );
 }
