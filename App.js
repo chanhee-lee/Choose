745d9a10dace2a0fcc299a083c9c96ffae981204
chanhee-lee <clee643@gatech.edu>
2020-11-06 21:42:31 -0500

    basic structure setup

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Menu from './src/view/screens/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Menu />
     </View>
   );
 }
