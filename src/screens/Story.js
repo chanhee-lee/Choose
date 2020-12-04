65cc5d81e8e684280a2762d4a004781b855e9fff
Joshua Kim <dearjoshua98@gmail.com>
2020-12-03 20:44:39 -0500

    Improve Death Screen & Link Death Screen to Story

--- src/screens/Story.js
+++ src/screens/Story.js
@@ -5,36 +5,75 @@ import Log from '../components/Log';
 import Choices from '../components/Choices';
 import StoryMap from '../model/Tree';
 import Color from '../constants';
+import Death from '../screens/Death';
 
 const Story = ({navigation, route}) => {
   const [logs, setLogs] = useState([]);
   const [currentNode, setCurrentNode] = useState(StoryMap.get('0'));
+  const [checkpoint, setCheckpoint] = useState(currentNode);
+  const [deathCount, setDeathCount] = useState(0);
+  const [showDeathScreen, setShowDeathScreen] = useState(false);
 
   const choiceHandler = (node) => {
     setCurrentNode(StoryMap.get(node.id)); // Update Node
-    addWordHandler(node); // Update log
-  }
+
+    // Updates checkpoint
+    if (currentNode?.savePoint) {
+      checkpointHandler(StoryMap.get(currentNode.savePoint[0]));
+    }
+
+    if (currentNode?.isDeath) {
+      deathCountHandler();
+      deathScreenHandler(true);
+      setCurrentNode(checkpoint);
+    } else {
+      addWordHandler(node); // Update log
+    }
+  };
 
   const addWordHandler = (node) => {
     setLogs(prevLogs => {
       return [...prevLogs, node];
     });
   };
-  return (
-    <View style={styles.container}>
-      <Log
-        data={logs}
-        font={route.params.font}
-      />
-      <Choices
-        children={currentNode?.children}
-        onPress={choiceHandler}
-        storyMap={StoryMap}
-        font={route.params.font}
-      />
-    </View>
-  )
-}
+  
+  const checkpointHandler = (node) => {
+    setCheckpoint(node);
+  };
+
+  const deathCountHandler = () => {
+    setDeathCount(prevCount => prevCount + 1);
+
+    // Resets game after 9 deaths
+    if (deathCount === 9) {
+      setDeathCount(0);
+    }
+  };
+
+  const deathScreenHandler = (val) => {
+    setShowDeathScreen(val);
+  };
+
+  return showDeathScreen ? 
+    <Death
+      deathCount={deathCount}
+      showDeathScreen={deathScreenHandler}
+    /> : 
+    (
+      <View style={styles.container}>
+        <Log
+          data={logs}
+          font={route.params.font}
+        />
+        <Choices
+          children={currentNode?.children}
+          onPress={choiceHandler}
+          storyMap={StoryMap}
+          font={route.params.font}
+        />
+      </View>
+    )
+};
 
 const styles = StyleSheet.create({
   container: {
